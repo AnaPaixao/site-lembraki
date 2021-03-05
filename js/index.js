cef176c5b820c935956ea2dfa9a0e052117b188c
Ana Paixão <aninha.beel@gmail.com>
2021-03-04 22:55:57 -0300

    pontuação no formulário e modificações na sessão team

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
+const star = document.querySelectorAll(".star");
+
+
 document.querySelectorAll("a").forEach(a => a.addEventListener("mouseover", () => a.classList.add("border-bottom")))
 
 document.querySelectorAll("a").forEach(a => a.addEventListener("mouseout", () => a.classList.remove("border-bottom")))
 
+star.forEach(star => star.addEventListener("mouseover", () => {
+    if (star.classList.contains("full") == false) {
+        star.classList.add("full")
+        return star.src = "../img/star.svg"
+
+    } else {
+        star.classList.remove("full")
+        return star.src = "../img/empty-star.svg"
+    }
+}))
